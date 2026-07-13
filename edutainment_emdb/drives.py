67c206ccc22e62f13b6ebef10014f728b07289ec
Antonio Leis <antoniojose.leis@gmail.com>
2026-07-13 11:59:40 +0200

    Added drives.py with DriveExponentialOpo

---
+++ edutainment_emdb/drives.py
@@ -0,0 +1,29 @@
+import rclpy
+from copy import copy
+from cognitive_nodes.drive import DriveTopicInput, Drive
+from builtin_interfaces.msg import Time as TimeMsg
+import random
+from core.utils import class_from_classname
+from math import exp, isclose
+
+class DriveExponentialOpo(DriveTopicInput):
+    def evaluate(self, perception=None):
+        """
+        Evaluates the drive value according to an inverted exponential function.
+
+        :param perception: The given normalized perception.
+        :type perception: dict
+        :return: The valuation of the perception and its timestamp.
+        :rtype: cognitive_node_interfaces.msg.Evaluation
+        """
+        self.old_evaluation = copy(self.evaluation)
+        if self.input >= 0:
+            a = 1-self.min_eval
+            self.evaluation.evaluation = a*exp(-5*self.input) + self.min_eval
+            if isclose(self.input, 1.0, ):
+                self.evaluation.evaluation = 1.0
+        else:
+            self.evaluation.evaluation = 0.0
+        self.evaluation.timestamp = self.get_clock().now().to_msg()
+
+        return self.evaluation
