752a3fffbc62c64ed14901e1dc1e91f6d6cbb46c
Antonio Leis <antoniojose.leis@gmail.com>
2026-07-15 13:34:43 +0200

    Added idle Policy0

--- edutainment_emdb/pnodes.py
+++ edutainment_emdb/pnodes.py
@@ -16,6 +16,43 @@ BORED_HIGH_THRESHOLD = 0.75
 STANDING_LOW_THRESHOLD = 0.5
 STANDING_HIGH_THRESHOLD = 0.75
 
+class PNodeIdle(PNode): #Pn0
+    """
+    PNode that represents if the system should be idle or not. It is activated when the python_errors_perception is below the low threshold.
+    """
+    def __init__(self, name='idle_pnode', class_name='cognitive_nodes.pnode.PNode', space_class=None, space=None, history_size=100, **params):
+        super().__init__(name, class_name, space_class, space, history_size, **params)
+    def calculate_activation(self, perception=None, activation_list=None):
+        """
+        Calculate the new activation value for a given perception.
+
+        :param perception: The perception for which P-Node activation is calculated.
+        :type perception: dict
+        :param activation_list: The list of activations to be used for the calculation.
+        :type activation_list: list
+        :return: If there is space, returns the activation of the P-Node. If not, returns 0. 
+            It also returs the timestamp.
+        :rtype: cognitive_node_interfaces.msg.Activation
+        """
+        if activation_list!=None:
+            perception={}
+            for sensor in activation_list:
+                activation_list[sensor]['updated']=False
+                perception[sensor]=activation_list[sensor]['data']
+        if perception:
+            value_raw = perception.get('python_errors_perception')
+            value = round(value_raw[0]['data'], 1)
+
+            # Pn0: Python errors < low threshold
+            if value < PYTHON_ERRORS_LOW_THRESHOLD:
+                self.activation.activation = 0.95
+                self.get_logger().debug(f"PNODE DEBUG: idle_pnode: {value}")
+            else:
+                self.activation.activation = 0.0
+            
+            self.activation.timestamp = self.get_clock().now().to_msg()
+        return self.activation
+
 class PNodePythonErrors(PNode): #Pn1.1
     """
     PNode that represents Python errors
--- edutainment_emdb/policies.py
+++ edutainment_emdb/policies.py
@@ -4,6 +4,14 @@ import random
 from cognitive_nodes.policy import Policy
 import requests
 
+class PolicySkill0(Policy): # Dummy idle policy.
+    def __init__(self, name='skill_0_idle_policy', class_name='cognitive_nodes.policy.Policy', publisher_msg=None, publisher_topic=None, **params):
+        super().__init__(name, class_name, publisher_msg, publisher_topic, **params)
+    async def execute_callback(self, request, response):
+        #requests.post("http://localhost:2727/api/skills/0")
+        response.policy = self.name
+        return response
+
 class PolicySkill1(Policy):
     def __init__(self, name='skill_1_python_policy', class_name='cognitive_nodes.policy.Policy', publisher_msg=None, publisher_topic=None, **params):
         super().__init__(name, class_name, publisher_msg, publisher_topic, **params)
