ef8c68f4345348bff3b238c416e85c65063f34e9
Antonio Leis <antoniojose.leis@gmail.com>
2026-07-22 13:02:21 +0200

    Changed skill calls from requests to ROS2 services

--- edutainment_emdb/policies.py
+++ edutainment_emdb/policies.py
@@ -1,14 +1,37 @@
 import time
 import rclpy
 import random
-from cognitive_nodes.policy import Policy
 import requests
+from cognitive_nodes.policy import Policy
+from rclpy.node import Node
+from std_srvs.srv import Empty
+
+class ServiceClient(Node):
+    def __init__(self, name='skill1_client', service_name='pillar_control/skill_1'):
+        super().__init__(name)
+        self.service_name = service_name
+        self.client = self.create_client(Empty, self.service_name)
+        while not self.client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info(f'Waiting for service {self.service_name}')
+        self.request = Empty.Request()
+
+    def call_service(self):
+        future = self.client.call_async(self.request)
+        rclpy.spin_until_future_complete(self, future)
+        if future.result() is not None:
+            self.get_logger().info(f'Called {self.service_name}')
+        else:
+            self.get_logger().error(f'Error while calling service: {future.exception()}')
+
 
 class PolicySkill0(Policy): # Dummy idle policy.
     def __init__(self, name='skill_0_idle_policy', class_name='cognitive_nodes.policy.Policy', publisher_msg=None, publisher_topic=None, **params):
         super().__init__(name, class_name, publisher_msg, publisher_topic, **params)
     async def execute_callback(self, request, response):
         #requests.post("http://localhost:2727/api/skills/0")
+        #node = ServiceClient(name=self.name, service_name='pillar_control/skill_0')
+        #node.call_service()
+        #node.destroy_node()
         response.policy = self.name
         return response
 
@@ -16,7 +39,10 @@ class PolicySkill1(Policy):
     def __init__(self, name='skill_1_python_policy', class_name='cognitive_nodes.policy.Policy', publisher_msg=None, publisher_topic=None, **params):
         super().__init__(name, class_name, publisher_msg, publisher_topic, **params)
     async def execute_callback(self, request, response):
-        requests.post("http://localhost:2727/api/skills/1")
+        #requests.post("http://localhost:2727/api/skills/1")
+        node = ServiceClient(name=self.name, service_name='pillar_control/skill_1')
+        node.call_service()
+        node.destroy_node()
         response.policy = self.name
         return response
 
@@ -24,7 +50,10 @@ class PolicySkill2(Policy):
     def __init__(self, name='skill_2_evaluation_policy', class_name='cognitive_nodes.policy.Policy', publisher_msg=None, publisher_topic=None, **params):
         super().__init__(name, class_name, publisher_msg, publisher_topic, **params)
     async def execute_callback(self, request, response):
-        requests.post("http://localhost:2727/api/skills/2")
+        #requests.post("http://localhost:2727/api/skills/2")
+        node = ServiceClient(name=self.name, service_name='pillar_control/skill_2')
+        node.call_service()
+        node.destroy_node()
         response.policy = self.name
         return response
 
@@ -32,7 +61,10 @@ class PolicySkill3(Policy):
     def __init__(self, name='skill_3_teacher_policy', class_name='cognitive_nodes.policy.Policy', publisher_msg=None, publisher_topic=None, **params):
         super().__init__(name, class_name, publisher_msg, publisher_topic, **params)
     async def execute_callback(self, request, response):
-        requests.post("http://localhost:2727/api/skills/3")
+        #requests.post("http://localhost:2727/api/skills/3")
+        node = ServiceClient(name=self.name, service_name='pillar_control/skill_3')
+        node.call_service()
+        node.destroy_node()
         response.policy = self.name
         return response
 
@@ -40,7 +72,10 @@ class PolicySkill4(Policy):
     def __init__(self, name='skill_4_sit_policy', class_name='cognitive_nodes.policy.Policy', publisher_msg=None, publisher_topic=None, **params):
         super().__init__(name, class_name, publisher_msg, publisher_topic, **params)
     async def execute_callback(self, request, response):
-        requests.post("http://localhost:2727/api/skills/4")
+        #requests.post("http://localhost:2727/api/skills/4")
+        node = ServiceClient(name=self.name, service_name='pillar_control/skill_4')
+        node.call_service()
+        node.destroy_node()
         response.policy = self.name
         return response
 
@@ -48,7 +83,10 @@ class PolicySkill5(Policy):
     def __init__(self, name='skill_5_break_policy', class_name='cognitive_nodes.policy.Policy', publisher_msg=None, publisher_topic=None, **params):
         super().__init__(name, class_name, publisher_msg, publisher_topic, **params)
     async def execute_callback(self, request, response):
-        requests.post("http://localhost:2727/api/skills/5")
+        #requests.post("http://localhost:2727/api/skills/5")
+        node = ServiceClient(name=self.name, service_name='pillar_control/skill_5')
+        node.call_service()
+        node.destroy_node()
         response.policy = self.name
         return response
 
@@ -56,7 +94,10 @@ class PolicySkill6(Policy):
     def __init__(self, name='skill_6_video_policy', class_name='cognitive_nodes.policy.Policy', publisher_msg=None, publisher_topic=None, **params):
         super().__init__(name, class_name, publisher_msg, publisher_topic, **params)
     async def execute_callback(self, request, response):
-        requests.post("http://localhost:2727/api/skills/6")
+        #requests.post("http://localhost:2727/api/skills/6")
+        node = ServiceClient(name=self.name, service_name='pillar_control/skill_6')
+        node.call_service()
+        node.destroy_node()
         response.policy = self.name
         return response
 
@@ -64,6 +105,9 @@ class PolicySkill7(Policy):
     def __init__(self, name='skill_7_gamification_policy', class_name='cognitive_nodes.policy.Policy', publisher_msg=None, publisher_topic=None, **params):
         super().__init__(name, class_name, publisher_msg, publisher_topic, **params)
     async def execute_callback(self, request, response):
-        requests.post("http://localhost:2727/api/skills/7")
+        #requests.post("http://localhost:2727/api/skills/7")
+        node = ServiceClient(name=self.name, service_name='pillar_control/skill_7')
+        node.call_service()
+        node.destroy_node()
         response.policy = self.name
         return response
