5abf204d9d09068e366f1e7522d92049314f7cca
Antonio Leis <antoniojose.leis@gmail.com>
2026-08-03 13:53:36 +0200

    Added missing configure_activation_inputs

--- edutainment_emdb/world_models.py
+++ edutainment_emdb/world_models.py
@@ -15,6 +15,7 @@ STUDENT_TYPE_EXPERT_THRESHOLD = 0.8
 class EdutainmentStudentExpert(WorldModel):
     def __init__(self, name='EXPERT_WM', class_name='cognitive_nodes.world_model.WorldModel', **params):
         super().__init__(name=name, class_name=class_name, **params)
+        self.configure_activation_inputs(self.neighbors)
         self.perception = None
         self.cbgroup_activation = getattr(self, "cbgroup_activation", MutuallyExclusiveCallbackGroup())
 
@@ -93,6 +94,7 @@ class EdutainmentStudentExpert(WorldModel):
 class EdutainmentStudentAmateur(WorldModel):
     def __init__(self, name='AMATEUR_WM', class_name='cognitive_nodes.world_model.WorldModel', **params):
         super().__init__(name=name, class_name=class_name, **params)
+        self.configure_activation_inputs(self.neighbors)
         self.selected_behavior = None
         self.configure_activation_inputs(self.neighbors)
 
@@ -171,6 +173,7 @@ class EdutainmentStudentAmateur(WorldModel):
 class EdutainmentStudentGeneral(WorldModel):
     def __init__(self, name='GENERAL_WM', class_name='cognitive_nodes.world_model.WorldModel', **params):
         super().__init__(name=name, class_name=class_name, **params)
+        self.configure_activation_inputs(self.neighbors)
         self.selected_behavior = None
         self.configure_activation_inputs(self.neighbors)
 
